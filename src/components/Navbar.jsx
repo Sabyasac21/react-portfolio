8dbcbf3518abced8f1357052030c6a4ec5e4b6cf
Sabyasachi Nishant <Sabyasachi.Nishant@almonds.ai>
2024-10-01 14:30:20 +0530

    minor commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import logo from "../assets/kevinRushLogo.png";
-import { FaLinkedin, FaGithub, FaTwitter, FaInstagram } from 'react-icons/fa';
+import { FaLinkedin, FaGithub, FaCode, FaGraduationCap } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
     <nav className=' mb-20 flex items-center justify-between py-6'>
-        <div className="flex flex-shrink-0 items-center">
-            <img className='mx-2 w-10' src={logo} alt=''/>
-        </div>
-        <div className='m-8 flex items-center gap-4 text-2xl'>
-            <FaLinkedin/>
-            <FaGithub/>
-            <FaInstagram/>
-            <FaTwitter/>
-        </div>
+      <div className="flex flex-shrink-0 items-center">
+        <img className='mx-2 w-10' src={logo} alt='' />
+      </div>
+      <div className='m-8 flex items-center gap-4 text-2xl'>
+        <a href="https://www.linkedin.com/in/sabyasachi-nishant-1843761b1"><FaLinkedin /></a>
+        <a href="https://github.com/Sabyasac21"><FaGithub /></a>
+        <a href="https://leetcode.com/u/sabyasachigolu/"><FaCode /></a>
+        <a href='https://www.scaler.com/academy/profile/3f8bb943f018/'><FaGraduationCap/></a>
+
+
+      </div>
     </nav>
   )
 }
 
-export default Navbar
+export default Navbar 
