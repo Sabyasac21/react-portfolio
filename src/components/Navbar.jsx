598fc5363193fa24aa1d793a9d310265e16ac9b3
Sabyasachi Nishant <sabyasachi.nishant@vengage.ai>
2025-07-06 13:48:31 +0530

    changed logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from "../assets/kevinRushLogo.png";
+import logo from "../assets/portfolio.png";
 import { FaLinkedin, FaGithub, FaCode, FaGraduationCap } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
     <nav className=' mb-20 flex items-center justify-between py-6'>
       <div className="flex flex-shrink-0 items-center">
-        <img className='mx-2 w-10' src={logo} alt='' />
+        <img className='mx-2 w-28 h-28' src={logo} alt='' />
       </div>
       <div className='m-8 flex items-center gap-4 text-2xl'>
         <a href="https://www.linkedin.com/in/sabyasachi-nishant-1843761b1"><FaLinkedin /></a>
